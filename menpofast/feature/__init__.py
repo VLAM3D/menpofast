6bd6847450d4fe7ca3783095ce98bbc7ef08cf88
Joan Alabort <ja310@imperial.ac.uk>
2014-11-11 09:21:25 +0000

    Change predifined feature names

--- menpofast/feature/__init__.py
+++ menpofast/feature/__init__.py
@@ -1,3 +1,3 @@
 from .features import no_op, gradient, gaussian_filter, daisy, dsift
-from .predefined import aam_daisy, aam_dsift
+from .predefined import fast_daisy, fast_dsift
 from .base import ndfeature, imgfeature
